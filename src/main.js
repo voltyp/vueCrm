ffd97e0bcb2ed8f943de2181577963911a9bdfa0
Yuri <voltermaher@gmail.com>
2020-12-24 02:20:58 +0700

    add validate form login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from "vuelidate/src"
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -6,7 +7,9 @@ import store from './store'
 import 'materialize-css/dist/js/materialize.min'
 import dateFilter from "@/filters/date.filter"
 
+
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
 new Vue({
