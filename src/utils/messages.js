d9b3c1cc25c2b20ed444f4d09e9603f1a628375e
Yuri <voltermaher@gmail.com>
2020-12-26 19:30:36 +0700

    adding registration

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-    'logout': 'Вы вышли из системы'
+    'logout': 'Вы вышли из системы',
+    'auth/user-not-found': 'Пользователь c таким Email не найден',
+    'auth/wrong-password': 'Неверный пароль',
+    'auth/email-already-in-use': 'Email уже занят'
 }
