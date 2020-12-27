72818271db9e8ce12f6cbd6da5225c27ba3531a8
Yuri <voltermaher@gmail.com>
2020-12-27 17:41:18 +0700

    api connection

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import router from './router'
 import store from './store'
 import 'materialize-css/dist/js/materialize.min'
 import dateFilter from "@/filters/date.filter"
+import currencyFilter from "@/filters/currency.filter";
 import messagePlugin from '@/utils/message.plugin'
-
+import Loader from '@/components/app/Loader'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
@@ -16,6 +17,8 @@ Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 firebase.initializeApp({
   apiKey: "AIzaSyAlS8HhO6aeMexyl4pdtfd1rTZbQ_3Fy1U",
